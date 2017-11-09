715b23bfe0e96fbc9ca4a39b1ae9f956aaa5aec3
jayhomz <johnstonewahome@gmail.com>
2017-11-09 12:41:14 +0300

    added working html page to the site

--- myblog/views.py
+++ myblog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. This goes to the templates folder and searches for blog/post_list.html
+def post_list(request):
+    return render(request, 'blog/post_list.html')
