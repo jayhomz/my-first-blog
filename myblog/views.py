bf7054ff56fc370b606b6833f169969b73500321
jayhomz <johnstonewahome@gmail.com>
2017-11-09 13:11:10 +0300

    made the html page take post from the database

--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here. This goes to the templates folder and searches for blog/post_list.html
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
